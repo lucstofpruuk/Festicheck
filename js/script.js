beb78ca0342ef188d39e2f515c0ea54c6b2771bd
Luc <luc.melissant@outlook.com>
2018-06-07 17:00:17 +0200

    Versie 2.0
    Einde van de dag en week. Dus WEEEKEND!!!

--- js/script.js
+++ js/script.js
@@ -18,9 +18,13 @@ $(".register").click(function(){
 document.getElementById('x').addEventListener('click', function () {
     if (this.classList.contains('clicked')) {
       document.getElementById("myNav").style.height = "0%";
+      document.getElementById("body").style.overflow = "auto";
+      document.getElementById("nav").style.backgroundColor = "rgba(0,0,0, 0.6)";
       this.classList.remove('clicked');
     } else {
       this.classList.add('clicked');
       document.getElementById("myNav").style.height = "100%";
+      document.getElementById("body").style.overflow = "hidden";
+      document.getElementById("nav").style.backgroundColor = "#F15024";
     }
   }); 
